69df5550082de2065ecc41d7ab7a84bb7709e0da
denyanwardev <denyanwardev@gmail.com>
2024-08-21 16:13:27 +0800

    function

---
+++ bin/dart_fundamental/for_in.dart
@@ -0,0 +1,13 @@
+void main() {
+  var names = <String>['Deny', 'Rachel', 'Anwar'];
+
+  for (var value in names) {
+    print(value);
+  }
+
+  var namesSet = <String>{'Deny', 'Rachel', 'Anwar'};
+
+  for (var value in namesSet) {
+    print(value);
+  }
+}
